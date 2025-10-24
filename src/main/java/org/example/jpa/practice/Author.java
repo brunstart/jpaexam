423a53ca316792a3b6033d2290166946660b515e
brunstart <brunstart@gmail.com>
2025-10-24 13:16:46 +0900

    JPA 실습, 다대일 관계

--- src/main/java/org/example/jpa/practice/Author.java
+++ src/main/java/org/example/jpa/practice/Author.java
@@ -16,6 +16,7 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "author", cascade=CascadeType.ALL, orphanRemoval = true)
--- src/main/java/org/example/jpa/practice/BookMain.java
+++ src/main/java/org/example/jpa/practice/BookMain.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.jpa.JPAUtil;
 
 @Slf4j
-public class AuthorMain {
+public class BookMain {
 
     private static void find() {
         EntityManager em = JPAUtil.getEntityManagerFactory().createEntityManager();
